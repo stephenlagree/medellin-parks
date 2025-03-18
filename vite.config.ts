0cdd878aaa9af13f86ede522dd45df435a9ee2a7
slagree <stephen.lagree@gmail.com>
2025-03-17 23:26:08 -0400

    Fix: Update paths for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://stephenlagree.github.io/medellin-parks/',
+  base: '/medellin-parks/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -16,7 +16,12 @@ export default defineConfig({
         },
         entryFileNames: 'assets/[name].[hash].js',
         chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        assetFileNames: ({ name }) => {
+          if (name === 'vite.svg') {
+            return '[name].[ext]';
+          }
+          return 'assets/[name].[hash].[ext]';
+        }
       },
     },
   },
